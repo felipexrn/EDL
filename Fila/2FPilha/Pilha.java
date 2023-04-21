9917645dd7a8e601cc459a5bfc6f798f5dc4d90f
felipexrn <112009958+felipexrn@users.noreply.github.com>
2023-04-21 05:54:24 +0000

    Pilha com duas Filas

---
+++ Fila/2FPilha/Pilha.java
@@ -0,0 +1,62 @@
+public class Pilha implements IPilha {
+  Fila f1, f2;
+  int capacity, tx;
+  boolean FC;
+  public Pilha(int tam, boolean factor, int tax) {
+    f1 = new Fila(tam, factor, tax);
+    f2 = new Fila(tam, factor, tax);
+    capacity = tam;
+    FC = factor;
+    tx = tax;
+  }
+  public int size() {
+    return f1.size();
+  }   
+  public boolean isEmpty() {
+    return f1.isEmpty();
+  }
+  public boolean isFull() {
+    return f1.isFull();
+  }
+  public Object top(){
+    if (this.isEmpty()) throw new EmptyStackException("Pilha vazia"); 
+    else {
+      int len = f1.size();
+      for (int i = 0; i < len-1; i++) f2.enqueue(f1.dequeue());
+      Object temp = f1.dequeue();
+      len = f2.size();
+      for (int i = 0; i < len; i++) f1.enqueue(f2.dequeue());
+      return temp;
+    }
+  }
+  public void push(Object o) {
+    f1.enqueue(o);
+  }
+  public void adicionaPilha(Pilha P) {
+    Pilha P2 = new Pilha(capacity, FC, tx);
+    while(P.size() != 0) P2.push(P.pop());
+    while(P2.size() != 0) {
+      Object o = P2.pop();
+      this.push(o);
+      P.push(o);
+    } 
+  }
+  public Object pop() {
+    if (this.isEmpty()) throw new EmptyStackException("Pilha vazia");
+    else {
+      int len = f1.size();
+      for (int i = 0; i < len-1; i++) f2.enqueue(f1.dequeue());
+      Object temp = f1.first();
+      len = f2.size();
+      for (int i = 0; i < len; i++) f1.enqueue(f2.dequeue());
+      return temp;
+    }
+  }
+  public void empty() {
+    while (!f1.isEmpty()) f1.dequeue();
+  }
+  public String toString(){
+    return f1.toString();
+  }
+}
+
---
+++ Fila/2FPilha/testePilha.java
@@ -0,0 +1,43 @@
+public class testePilha {
+	public static void main(String[] args) {
+    int in = 10, out = 5, tam = 10, tax = 10;
+    String[] nomesPilhas = {"p1", "p2"};
+		Pilha p1 = new Pilha(tam, true, tax);
+		System.out.println("inserindo " + in + " elementos na pilha " + nomesPilhas[0]);
+		for(int i = 0; i < in; i++){
+      p1.push(i);
+		  System.out.println(i);		  
+		}
+    p1.print();
+		System.out.println("retirando " + in + " elementos na pilha " + nomesPilhas[0]);
+    System.out.println("topo - retirado");
+		for(int i = 0; i < out; i++){  
+			System.out.print(p1.top());
+      System.out.print(" - ");
+      System.out.println(p1.pop());
+		}
+    System.out.println("Estado inicial da pilha " + nomesPilhas[0] + " antes do empty()");	
+    p1.print();
+    p1.empty();
+    System.out.println("Estado final da pilha " + nomesPilhas[0] + " após o empty()");	
+    p1.print();
+    System.out.println("inserindo " + out + " elementos na pilha " + nomesPilhas[0]);
+		for(int i = 0; i < out; i++){
+      p1.push(i);
+		  System.out.println(i);		  
+		}
+    PilhaArray p2 = new PilhaArray(1, false, 10);
+    System.out.println("inserindo " + out + " elementos na pilha " + nomesPilhas[1]);
+		for(int i = 0; i < out; i++){
+      p2.push(i);
+		  System.out.println(i);		  
+		}
+    System.out.println("Estado inicial das pilhas " + nomesPilhas[0] +  " e " + nomesPilhas[1] + " antes do adiconaPilha()");	
+    p2.print();
+    p1.print();
+    p1.adicionaPilha(p2);
+    System.out.println("Estado final das pilhas " + nomesPilhas[0] +  " e " + nomesPilhas[1] + " depois do adiconaPilha()");	
+    p2.print();
+    p1.print();
+	}
+}
