92b3ed67b192b34f20f728ddba320100d94bd469
felipexrn <112009958+felipexrn@users.noreply.github.com>
2023-07-15 18:28:15 +0000

    Ajustes de exibição

--- PigeonHole/testePigeonHoleSort.java
+++ PigeonHole/testePigeonHoleSort.java
@@ -1,27 +1,9 @@
 import java.util.ArrayList;
-import java.util.Random;
 public class testePigeonHoleSort {
   public static void main (String[] args) {
     pigeonHoleSort phs = new pigeonHoleSort();
     ArrayList<ArrayList<Object>> pigeonsHoles = new ArrayList<ArrayList<Object>>();
     ArrayList<Object> pigeons = new ArrayList<Object>();
-
-    // teste de eficiencia de tempo
-    Random num = new Random();
-    int range = 1000;
-    
-    for (int i = 0; i < range; i++) {
-      pigeons.add(num.nextInt(range));
-    }
-
-    long inicio = System.currentTimeMillis();
-    pigeons = phs.getSortPigeons(pigeons);
-    long fim = System.currentTimeMillis();
-
-    System.out.println("Para ordenar " + range + " pombos, o tempo foi de " + (fim-inicio) + " ms\n");
-
-    // Exibição dos dados
-    pigeons = new ArrayList<Object>();
     
     pigeons.add(8);
     pigeons.add(3);
