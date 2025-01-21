25f4fbf751795d9c47e4a97e900b4b8d3dd898c9
felipexrn <felipe.x@escolar.ifrn.edu.br>
2025-01-21 10:33:04 -0300

    deaclaradas funções do Grafo

--- Grafo/src/Grafo.java
+++ Grafo/src/Grafo.java
@@ -1,4 +1,45 @@
 package Grafo.src;
 public class Grafo {
-    
+    private ArrayList<Aresta> arestas;
+    private ArrayList<Vertice> vertices;
+
+    // Retorna um array armazenando os vértices finais da aresta e.
+    public ArrayList finalVertices(e) {
+        return null;
+    }
+
+    // Retorna o vértice oposto de v em e, ou seja, o vértice final da 
+    // aresta e separado do vértice v. Um erro ocorre se e não é
+    // incidente a v
+    public Vertice oposto(v, e) {
+        return null;
+    }
+
+    // Retorna true se v e w são adjacentes
+    public Bool eAdjacente(v, w) {
+        return false;
+    }
+
+    // Substitui o elemento armazenado do vértice V com X
+    public void substituir(v, x) {
+
+    }
+
+    // Substitui o elemento armazenado na aresta e com x
+    public void substituir(e, x) {
+        
+    }
+
+    // Insere e retorna um novo vértice armazenando o elemento o
+    inserirVertice(o)
+
+    // Insere e retorna uma nova aresta não-dirigida (v,w) armazenando o elemento o
+    inserirAresta(v, w, o)
+
+    // Remove o vértice v ( e todas as arestas incidentes) e retorna o elemento armazenado em v
+    removeVértice(v)
+
+    // Remove a aresta e, retornando o elemento armazenado
+    removeAresta(e)
+
 }
